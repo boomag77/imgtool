3ef3d96a6217348351eb4aa77d3f271f64bbae09
boomag77 <s.burdakov@gmail.com>
2025-11-07 11:05:52 -0500

    Added abstractions

--- ImgViewer/Models/Abstractions.cs
+++ ImgViewer/Models/Abstractions.cs
@@ -10,9 +10,37 @@
         public string[] Files { get; set; }
     }
 
+    enum OperationType
+    {
+        Deskew,
+        BorderRemoval,
+        Binarize
+    }
 
+    public class Operation
+    {
+        private readonly OperationType type;
 
+    }
 
+    public class Pipeline
+    {
+        private Operation[] _operations;
+
+    }
+
+    public struct DeskewOperation
+    {
+        OperationType type;
+
+    }
+
+    enum DeskewAlgorithm
+    {
+        Auto,
+        ByBorders,
+
+    }
 
 }
 
